a8718e8633c6358e863dfd63b55e72290ca11f4e
Kent <kentsaeteurn@gmail.com>
2018-06-29 16:32:11 -0700

    make getWeatherInfo() a global function so that nested .ajax requests can call it with a new api; add error handling; get ipinfo of user to handle cases where user refuses to allow location access

--- script.js
+++ script.js
@@ -3,32 +3,41 @@ var fahrenheit;
 var celcius;
 
 function getLocation() {
-  navigator.geolocation.getCurrentPosition(showCoords);
+  navigator.geolocation.getCurrentPosition(showCoords, error);
+
+  function getWeatherInfo(json) {
+    $('#loadingMsg').show();
+
+    var city = json.name;
+    var state = json.sys.country;
+    temp = json.main.temp;
+    fahrenheit = Math.round((((temp * 9) / 5) + 32) * 10) / 10;
+    var forecast = json.weather[0].main;
+    var icon = json.weather[0].icon;
+
+    $("#title").html("Today's Weather");
+    $("#location").html(city + ", " + state);
+    $('#temperature').html('<span id="temp"></span><button onclick="switchScale()"><span id="scale">F</span></button>');
+    $("#temp").html(fahrenheit);
+    $("#forecast").html(forecast);
+
+    var img = new Image();
+    img.src = icon;
+    $("#icon").html(img);
+  }
 
   function showCoords(pos) {
     var lon = pos.coords.longitude;
     var lat = pos.coords.latitude;
     var api = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+lon;
 
-    $.getJSON(api, function(json){
-      $('#loadingMsg').show();
-
-      var city = json.name;
-      var state = json.sys.country;
-      temp = json.main.temp;
-      fahrenheit = Math.round((((temp * 9) / 5) + 32) * 10) / 10;
-      var forecast = json.weather[0].main;
-      var icon = json.weather[0].icon;
-
-      $("#title").html("Today's Weather");
-      $("#location").html(city + ", " + state);
-      $('#temperature').html('<span id="temp"></span><button onclick="switchScale()"><span id="scale">F</span></button>');
-      $("#temp").html(fahrenheit);
-      $("#forecast").html(forecast);
-
-      var img = new Image();
-      img.src = icon;
-      $("#icon").html(img);
+    $.ajax({
+      dataType: "jsonp",
+      type: "GET",
+      url: api,
+      success: function(json) {
+        getWeatherInfo(json);
+      }
     })
     .done(function() {
       $('#loadingMsg').hide();
@@ -38,6 +47,42 @@ function getLocation() {
       $('#content').html("Could not load the weather data :(")
     });
   };
+
+  function error(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+      case error.POSITION_UNAVAILABLE:
+      case error.TIMEOUT:
+      case error.UNKNOWN_ERROR:
+        $.ajax({
+          dataType: "jsonp",
+          type: "GET",
+          url: "https://ipinfo.io",
+          success: function(json) {
+            var coordinates = json.loc.split(",");
+            var lat = coordinates[0];
+            var lon = coordinates[1];
+            api = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+lon;
+            $.ajax({
+              dataType: "jsonp",
+              type: "GET",
+              url: api,
+              success: function(json) {
+                getWeatherInfo(json);
+              }
+            })
+            .done(function() {
+              $('#loadingMsg').hide();
+            })
+            .fail(function() {
+              $('#loadingMsg').hide();
+              $('#content').html("Could not load the weather data :(")
+            });
+          }
+        });
+        break;
+    }
+  }
 };
 
 function convertToCelcius() {
